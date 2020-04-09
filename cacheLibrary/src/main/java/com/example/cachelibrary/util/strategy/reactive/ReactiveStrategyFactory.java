b07dd6d98807eb1193afde06d354cc159957d6f4
maxidetorres <maxidetorres@gmail.com>
2020-04-09 16:39:17 -0300

    -Se formateo el codigo con java-google-format

--- cacheLibrary/src/main/java/com/example/cachelibrary/util/strategy/reactive/ReactiveStrategyFactory.java
+++ cacheLibrary/src/main/java/com/example/cachelibrary/util/strategy/reactive/ReactiveStrategyFactory.java
@@ -1,41 +1,36 @@
 package com.example.cachelibrary.util.strategy.reactive;
 
-
 import com.example.cachelibrary.util.strategy.reactive.ReactiveCacheControlEnum;
-
 import java.util.EnumMap;
 import java.util.Map;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class ReactiveStrategyFactory {
 
-    private Map<ReactiveCacheControlEnum, IReactiveStrategy> strategies =
-            new EnumMap<>(ReactiveCacheControlEnum.class);
+  private Map<ReactiveCacheControlEnum, IReactiveStrategy> strategies =
+      new EnumMap<>(ReactiveCacheControlEnum.class);
 
-    public ReactiveStrategyFactory() {
-        initStrategies();
-    }
+  public ReactiveStrategyFactory() {
+    initStrategies();
+  }
 
-    /**
-     * Verificar si existe el cache control y devolver la estragia concreta .
-     *
-     * @param cacheControl Nombre del cache control
-     * @return interface de la estrategia
-     */
-    public IReactiveStrategy getStrategy(ReactiveCacheControlEnum cacheControl) {
-        if (cacheControl == null || !strategies.containsKey(cacheControl)) {
-            return null;
-        }
-        return strategies.get(cacheControl);
+  /**
+   * Verificar si existe el cache control y devolver la estragia concreta .
+   *
+   * @param cacheControl Nombre del cache control
+   * @return interface de la estrategia
+   */
+  public IReactiveStrategy getStrategy(ReactiveCacheControlEnum cacheControl) {
+    if (cacheControl == null || !strategies.containsKey(cacheControl)) {
+      return null;
     }
+    return strategies.get(cacheControl);
+  }
 
-    /**
-     * Inicializar las estragias concretas .
-     */
-    private void initStrategies() {
-        strategies.put(ReactiveCacheControlEnum.NOSTORE, new ReactiveCacheControlNoStore());
-        strategies.put(ReactiveCacheControlEnum.MAXAGE, new ReactiveCacheControlMaxAge());
-    }
+  /** Inicializar las estragias concretas . */
+  private void initStrategies() {
+    strategies.put(ReactiveCacheControlEnum.NOSTORE, new ReactiveCacheControlNoStore());
+    strategies.put(ReactiveCacheControlEnum.MAXAGE, new ReactiveCacheControlMaxAge());
+  }
 }
